4bfe4075834dcd9b44e37c795d5fb349ad0fcc6a
sudhakarhue <82567301+sudhakarhue@users.noreply.github.com>
2022-01-24 15:56:42 +0530

    Add files via upload
    NodeJs_Docker_Example Project Created.

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    console.log("Test 1");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    console.log("Testing");
+    response.end("Hello World :: Docker Verification !");
+});
+
+ const port = process.env.PORT || 1337;
+ server.listen(port);
+ console.log("Server running at http://localhost:%d", port);
